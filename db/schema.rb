c9b18e9187060bb06990184cc9611dc91a56c769
2Deo <ziemowit.holda@gmail.com>
2017-10-18 19:11:37 +0200

    Adding migration AddArtistToSongs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018163555) do
+ActiveRecord::Schema.define(version: 20171018170828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20171018163555) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "songs", "artists"
 end

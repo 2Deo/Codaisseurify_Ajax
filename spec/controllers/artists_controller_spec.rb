2e903f68da90806700a40a82939bd064b8d4dd7c
2Deo <ziemowit.holda@gmail.com>
2017-10-19 21:47:49 +0200

    Testing Artist Controller

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ArtistsController, type: :controller do
 
+  describe "GET artists#index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end

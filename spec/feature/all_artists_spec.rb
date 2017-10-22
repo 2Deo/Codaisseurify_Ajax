906b943076f9288a4012619a6e602a862e2ec70a
2Deo <ziemowit.holda@gmail.com>
2017-10-22 19:54:35 +0200

    Updates

--- spec/feature/all_artists_spec.rb
+++ spec/feature/all_artists_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature 'Manage songs', js: true do
   scenario 'checks all artists button' do
 
-   # Point your browser towards the song path
     visit '/artists/'
 
     expect(page).to have_selector(:link_or_button, 'All Artists')

9ebad8fc2cc8dd7ad484215e4e6ce1e697a35027
2Deo <ziemowit.holda@gmail.com>
2017-10-22 19:11:40 +0200

    Tested for delete button

--- spec/feature/manage_songs_spec.rb
+++ spec/feature/manage_songs_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 feature 'Manage songs', js: true do
+
   scenario 'checks for delete option' do
 
-   # Point your browser towards the song path
-    visit '/artists/'
+    visit artists_path
 
-    expect(page).to have_selector(:link_or_button, 'All Artists')
-
-
-
-    # page.has_selector?('Delete')
-
+    page.has_selector?('Delete')
+    puts "Delete option tested"
   end
+
+
 end

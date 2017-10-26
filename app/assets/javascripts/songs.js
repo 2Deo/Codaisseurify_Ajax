e552071d8746a5119293e66962b57adee7f9eedf
2Deo <ziemowit.holda@gmail.com>
2017-10-26 20:24:53 +0200

    Show view minimalistic

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,52 +1,49 @@
-// $(function(){
-//   $("form").submit(function(event){
-//     event.preventDefault();
+// $(function () {
 //
-//     $.ajax({
-//       method: 'DESTROY',
-//       url: action,
-//       data: { title: title }
-//       dataType: 'script'
+//   var $songs = $('#all_songs');
 //
-//     }).done(function(data){
-//
-//     }).fail()
-//
-//     ;
+//   $.ajax({
+//     type: 'GET',
+//     url: '/api/songs',
+//     success: function(songs) {
+//       $.each(song, function(i, song) {
+//         $all_songs.append('<li>songs</li>');
+//       });
+//     }
 //   });
 // });
 
-function deleteSong(songId) {
-  $.ajax({
-    type: "DESTROY",
-    url: "api/songs/" + songId + ".json",
-    contentType: "application/json",
-    dataType: "json"
 
 
-  })
-  .done(function(data) {
-    $('li[data-id="'+songId+'"]').remove();
+<!-- -->
+  function deleteSong(artistId,songId) {
+    $.ajax({
+      type: "DELETE",
+      url: "/artists" + artistId + "/songs/" + songId,
+      contentType: "application/json",
+      dataType: "json"
+    })
+    .done(function(data) {
+      console.log();
+      $('#'+songId+'').remove();
+    });
+  }
+//
+// //-- create song
+  function createSong(songId) {
+    $.ajax({
+      type: "POST",
+      url: "api/songs.json",
+      data: json.stringify({
+        song: newSong
+      }),
+      contentType: "application/json",
+      dataType: 'json'
+    })
 
-    updateCounters();
+    .done(function(data) {
+      console.log(data);
+
+
   });
-}
-
-//-- create song
-function createSong(songId) {
-  $.ajax({
-    type: "POST",
-    url: "api/songs.json",
-    data: json.stringify({
-      song: newSong
-    }),
-    contentType: "application/json",
-    dataType: 'json'
-  })
-
-  .done(function(data) {
-    console.log(data);
-
-
-});
-}
+  }
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ApplicationRecord
-  belongs_to :artist
+  belongs_to :artist, optional: true
   validates :title, presence: true
 end

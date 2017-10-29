55d13893d98408fdb625075467f2ac8608892d7f
2Deo <ziemowit.holda@gmail.com>
2017-10-29 22:55:44 +0100

    Testing

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,32 +1,30 @@
 class SongsController < ApplicationController
 
 
-    def new
-      @song = Song.new
+  def new
+    @song = Song.new
+  end
+
+  def show
+    @song = @artist.songs.new(song_params)
+    @song = Song.new
+  end
+
+
+  def create
+    @artist = Artist.find(params[:artist_id])
+    @song = @artist.songs.create(song_params)
+
+    respond_to do |format|
+      if @song.save
+         format.html { redirect_to @artist }
+         format.json { render json: @song}
+       else
+        format.html { redirect_to @artist }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+       end
     end
-
-    def show
-      @song = @artist.songs.new(song_params)
-      @song = Song.new
-    end
-
-
-    def create
-      @artist = Artist.find(params[:artist_id])
-      @song = @artist.songs.create(song_params)
-
-      respond_to do |format|
-        if @song.save
-          format.html { redirect_to artist_path(@artist), notice: 'Song Created!' }
-          format.json { render :show, status: :created, location: @artist }
-          format.js
-        else
-          format.html { redirect_to root_path }
-          format.json { render json: @artist.errors, status: :unprocessable_entity }
-          format.js
-        end
-      end
-    end
+  end
 
 
 
@@ -43,7 +41,7 @@ class SongsController < ApplicationController
   end
 
   def song_params
-    song_params = params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_id)
   end
 
 end
